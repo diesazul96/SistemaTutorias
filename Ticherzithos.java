098f32d60b75e8fcc579014774014408950d3720
Diego Alvarez Monroy <diesazul96@hotmail.com>
2016-11-19 15:37:34 -0500

    Update Ticherzithos.java

--- Ticherzithos.java
+++ Ticherzithos.java
@@ -5,5 +5,32 @@ public abstract class Ticherzithos extends Pipoll{
         super(nombre, identificacion);
     }
     
+    private TingAkdemic[] Materias;
+    private String[] Horario;
+    private int numeroMaterias;
+
+    public TingAkdemic[] getMaterias() {
+        return Materias;
+    }
+
+    public void setMaterias(TingAkdemic[] Materias) {
+        this.Materias = Materias;
+    }
+
+    public String[] getHorario() {
+        return Horario;
+    }
+
+    public void setHorario(String[] Horario) {
+        this.Horario = Horario;
+    }
+
+    public int getNumeroMaterias() {
+        return numeroMaterias;
+    }
+
+    public void setNumeroMaterias(int numeroMaterias) {
+        this.numeroMaterias = numeroMaterias;
+    }
     
 }
