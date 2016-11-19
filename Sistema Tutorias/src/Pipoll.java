0e5c5e0e3788957cb6e31ff7b96b44a508ee1b8c
Diego Esteban Alvarez Monroy <diesazul96@hotmail.com>
2016-11-19 14:05:49 -0500

    Prueba

---
+++ Sistema Tutorias/src/Pipoll.java
@@ -0,0 +1,11 @@
+//Clase: Persona
+
+public class Pipoll {
+    protected String nombre;
+    protected String identificacion;
+
+    public Pipoll(String nombre, String identificacion) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+    }
+}
