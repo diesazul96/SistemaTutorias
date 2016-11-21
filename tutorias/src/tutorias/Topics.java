9dfc58c3ba0ff29898a59c90e1150fb9b59408f5
Diego Alvarez Monroy <diesazul96@hotmail.com>
2016-11-20 22:30:10 -0500

    Add files via upload

---
+++ tutorias/src/tutorias/Topics.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutorias;
+
+/**
+ *
+ * @author Diego Alvarez <diesazul96@hotmail.com>
+ */
+
+public interface Topics {
+    
+    String calcDif = "Calculo Diferencial";
+    String calcInt = "Calculo Integral";
+    String modMat = "Modelacion Matematica";
+    String sistUno = "Sistemas I";
+    String progApli = "Programacion Aplicada";
+    String catedra = "Catedra Rorigo Noguera Laborde";
+    String conta = "Contabilidad";
+    String teoriaSis = "Teoria General de Sistemas";
+    
+}
