af995db779f57f49ead7d1fe1e5491249eea256a
Diego Alvarez Monroy <diesazul96@hotmail.com>
2016-11-21 20:44:45 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -27,9 +27,13 @@ public class Main {
                         String nombreProfesor = lec.next();
                         System.out.println("Digite el número de identificación del profesor de planta a agregar");
                         String identificacionProfesor = lec.next();
-                        ProfesorPlanta pp = new ProfesorPlanta(nombreProfesor, identificacionProfesor);
+                        Ticherzithos pp = new ProfesorPlanta(nombreProfesor, identificacionProfesor);
                         System.out.println("Digite el nombre de la materia a agregar");
                         String nombreMateria = lec.next();
+                        System.out.println("Digite el numero de horas de la tutoria");
+                        String horas = lec.next();
+                        int salario = pp.calcularSalario(horas);
+                        System.out.println("El pago que se debe hacer por esta tutoria es de " + salario);
                         TingAkdemic m = new TingAkdemic(nombreMateria);
                         Tutorias tut = new Tutorias(pp, m);
                         System.out.println("Tutoría de "+m.getNombre()+" con el profesor(a) "+pp.getNombre()+" creada correctamente");
@@ -39,10 +43,14 @@ public class Main {
                         String nombreProfesor = lec.next();
                         System.out.println("Digite el número de identificación del profesor de cátedra a agregar");
                         String identificacionProfesor = lec.next();
-                        ProfesorCatedra pc = new ProfesorCatedra(nombreProfesor, identificacionProfesor);
+                        Ticherzithos pc = new ProfesorCatedra(nombreProfesor, identificacionProfesor);
                         System.out.println("Profesor de cátedra agregado correctamente");
                         System.out.println("Digite el nombre de la materia a agregar");
                         String nombreMateria = lec.next();
+                        System.out.println("Digite el numero de horas de la tutoria");
+                        String horas = lec.next();
+                        int salario = pc.calcularSalario(horas);
+                        System.out.println("El pago que se debe hacer por esta tutoria es de " + salario);
                         TingAkdemic m = new TingAkdemic(nombreMateria);
                         System.out.println("materia " + nombreMateria + " agregada correctamente");
                         Tutorias tut = new Tutorias(pc, m);
@@ -50,7 +58,7 @@ public class Main {
                         u.agregarTutoria(tut);
                     }
 
-                    System.out.println("¿Desea continuar en con el menú de administración de tutorías? (Pulse 1.Sí, 2.No)");
+                    System.out.println("¿Desea continuar en el menú de administración de tutorías? (Pulse 1.Sí, 2.No)");
                     int ans = lec.nextInt();
                     if (ans == 1) flag = true;
                     else flag = false;
